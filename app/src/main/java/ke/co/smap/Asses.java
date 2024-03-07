9b8fab60f823d860fe63dff2a223967d5c2e0f00
micro-byte <kelvinthuranira591@gmail.com>
2024-03-07 04:23:07 -0800

    imageUrl fixed

--- app/src/main/java/ke/co/smap/Asses.java
+++ app/src/main/java/ke/co/smap/Asses.java
@@ -140,7 +140,7 @@ public class Asses extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-
+//
 
         employee_consent.setOnClickListener(v -> {
             //Here call OpenGalleryMethod
